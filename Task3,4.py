f695fad2d8bdb9dd59d3c12eacab5778c7c89a4a
yanochkina_sl <yanochkinasvetlana@mail.ru>
2022-12-05 18:45:51 +0600

    Add task 4

--- Task3,4.py
+++ Task3,4.py
@@ -10,3 +10,30 @@ todo_list = TodoList()
 todo_list.tasks = ['Купить лампочку', 'Поменять лампочку', 'Выкинуть лампочку']
 
 print("\n".join(todo_list.tasks))
+
+# Задание 4
+
+class DataBase:
+    def __new__(cls, *args, **kwargs):
+        return
+
+    def __init__(self, user, psw, port):
+        self.user = user
+        self.psw = psw
+        self.port = port
+
+    def connect(self):
+        print(f"соединение с БД: {self.user}, {self.psw}, {self.port}")
+
+    def close(self):
+        print("закрытие соединения с БД")
+
+    def read(self):
+        return "данные из БД"
+
+    def write(self, data):
+        print(f"запись в БД {data}")
+
+db = DataBase('root', '1234', 80)
+db2 = DataBase('root2', '5678', 40)
+print(db is db2)
