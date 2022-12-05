4444c9d46f19a040f17514578647dc1c34d21093
yanochkina_sl <yanochkinasvetlana@mail.ru>
2022-12-05 17:53:41 +0600

    Add task2

--- Classes.py
+++ Classes.py
@@ -19,3 +19,28 @@ b3.volume = 1.0
 print(b1.color, b1.volume)
 print(b2.color, b2.volume)
 print(b3.color, b3.volume)
+
+# Задание 2
+
+class Bottle:
+    color = 'color'
+    contains = 0
+    def get_content(self):
+        return self.contains
+
+    def fill(self, volume):
+       self.contains += volume
+       return self.contains
+
+b1 = Bottle()
+b2 = Bottle()
+
+b1.color = 'Красная'
+b2.color = 'Синяя'
+
+print(b1.color, b1.get_content())
+b1.fill(100)
+print(b1.color, b1.get_content())
+print(b2.color, b2.get_content())
+b2.fill(500)
+print(b2.color, b2.get_content())
